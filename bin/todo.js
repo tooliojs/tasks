e9ca392350ced0e552b077898c5c47a744b34f8d
arakilian0 <arakilian0@gmail.com>
2020-05-10 00:43:10 -0700

    UPDATE cli script for testing

--- bin/todo.js
+++ bin/todo.js
@@ -1,23 +1,27 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk')
-const yargs = require('yargs')
-const task = require('../src')
+const path = require('path')
 
-try {
-    yargs.scriptName(chalk[task.config.cli.color]('todo'))
-    yargs.usage('$0 <cmd> [args]')
-    yargs.version(task.config.cli.version)
-    yargs.command('ls', 'list tasks', 
-        (yargs) => {}, 
-        (argv) => {
-            console.log('listing all tasks')
-        }
-    )
-    yargs.demandCommand(1, 'You need at least one command before moving on.')
-    yargs.strictCommands(true)
-    yargs.recommendCommands(true)
-    yargs.help(true)
-    yargs.argv
-}
-catch(err) { task.error(err) }
+console.log(path.join(__dirname))
+
+// const chalk = require('chalk')
+// const yargs = require('yargs')
+// const task = require('../src')
+
+// try {
+//     yargs.scriptName(chalk[task.config.cli.color]('todo'))
+//     yargs.usage('$0 <cmd> [args]')
+//     yargs.version(task.config.cli.version)
+//     yargs.command('ls', 'list tasks', 
+//         (yargs) => {}, 
+//         (argv) => {
+//             console.log('listing all tasks')
+//         }
+//     )
+//     yargs.demandCommand(1, 'You need at least one command before moving on.')
+//     yargs.strictCommands(true)
+//     yargs.recommendCommands(true)
+//     yargs.help(true)
+//     yargs.argv
+// }
+// catch(err) { task.error(err) }
