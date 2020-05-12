a4b19e8762ebcd176ac193cd80efdd493db4f875
arakilian0 <arakilian0@gmail.com>
2020-05-11 18:53:50 -0700

    UPDATE minor edit to config schema

--- src/config.js
+++ src/config.js
@@ -3,10 +3,7 @@ const Merge = require('../lib/Merge')
 module.exports.config = {
     entry: 'tasks.yml',
     agent: process.env.TOOLIO_AGENT,
-    cli: {
-        color: 'magenta',
-        version: require('../package.json').version
-    },
+    cliColor: 'magenta',
     error: {
         log: true,
         level: 0,
@@ -30,8 +27,9 @@ module.exports.config = {
             'Valid Entry Extension Required': '"entry" file extension is not valid',
             'Valid Entry Data Required:array': '"entry" file should be an array of tasks',
             'Valid Entry Data Required:object': '"tasks" need to be of type object',
+            'Valid New Title Required': 'new() expects a string but got undefined',
             'setConfig(option) Required': 'you tried calling "setConfig()" without any options',
-            'TypeError: chalk[task.config.cli.color] is not a function': 'invalid color given to option "cli.color"',
+            'TypeError: chalk[task.config.cliColor] is not a function': 'invalid color given to option "cli.color"',
             'TypeError: chalk[style.all] is not a function': 'invalid color given to option "error.style.color.all"',
             'TypeError: chalk[style.prefix] is not a function': 'invalid color given to option "error.style.color.prefix"',
             'TypeError: chalk[style.message] is not a function': 'invalid color given to option "error.style.color.message"'
